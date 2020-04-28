bfd5d9c4ecec4fddc62a4b364aafd451d58db3cd
chasakara <tinashechasakara@gmail.com>
2020-04-28 16:08:22 +0000

    changes to bmi-index else if statements

--- assets/js/BMI-index.js
+++ assets/js/BMI-index.js
@@ -22,16 +22,25 @@ function add() {
     var bmi = weight / (height * height);
 
     if (bmi <= 18.5) {
-      document.getElementById('result').innerHTML =(`Your BMI is:`, + bmi + `you are a underweight. It's likely that you're not consuming a healthy, balanced diet, which can lead to you lacking nutrients that your body needs to work properly. Healthy tip =>Choose nutrient-rich foods, try smoothies and shakes, watch when you drink, make every bite count, top it off, have an occasional treat and exercise.`);
+      document.getElementById('result').innerHTML =(`Your BMI is=>` + bmi + 
+                                                    `<br>It shows that you are a underweight. It's likely that you're not consuming a healthy, balanced diet, which can lead 
+                                                    to you lacking nutrients that your body needs to work properly. <br>
+                                                    Healthy tip => Choose nutrient-rich foods, try smoothies and shakes, watch when you drink, make every bite count, top it off, have an occasional treat and exercise.`);
     }
     else if ((bmi <= 24.9) && (bmi > 18.5)) {
-     document.getElementById('result').innerHTML =(`Well done! Your BMI is:`, + bmi + `show that you are a healthy weight. This means that you are less likely to develop heart disease, type 2 diabetes. Healthy tip =>keep eating well and tking regular exercise. It’s important to keep up these healthy habits.`);
+     document.getElementById('result').innerHTML =(`Your BMI is=>` + bmi + 
+                                                   `<br>It shows that you are a healthy weight. This means that you are less likely to develop heart disease, type 2 diabetes. <br>
+                                                   Healthy tip => Keep eating well and tking regular exercise. It’s important to keep up these healthy habits.`);
     }
     else if ((bmi <= 30) && (bmi > 24.9)) {
-      document.getElementById('result').innerHTML =(`Your BMI is:`, + bmi + `you are a overweight. You are consuming more calories than your body needs which it ends up storing as fat. Healthy tip =>you need to reduce the amount of calories you take daily a bit and exercise a little bit more.`);
+      document.getElementById('result').innerHTML =(`Your BMI is=>` + bmi + 
+                                                    `<br>Its shows that you are a overweight. You are consuming more calories than your body needs which it ends up storing as fat.<br>
+                                                     Healthy tip =>You need to reduce the amount of calories you take daily a bit and exercise a little bit more.`);
     }
     else if (bmi > 30) {
-     document.getElementById('result').innerHTML =(`Your BMI is:`, + bmi + `you are a obese. This means that you are more likely to develop heart disease, type 2 diabetes. You are consuming way more calories than your bodyneeds which it ends up storing as fat. Healthy tip =>you need to reduce the amount of calories you take daily and exercise more and try to have Whole grains, vegetables, whole fruits (not fruit juices),nuts, seeds, beans, and other healthful sources of protein.`);
+     document.getElementById('result').innerHTML =(`Your BMI is=>` + bmi + `<br>It shows that you are a obese. This means that you are more likely to develop heart disease, type 2 diabetes. <br>
+                                                   You are consuming way more calories than your bodyneeds which it ends up storing as fat. 
+                                                   <br>Healthy tip => You need to reduce the amount of calories you take daily and exercise more and try to have Whole grains, vegetables, whole fruits (not fruit juices),nuts, seeds, beans, and other healthful sources of protein.`);
     }
     
     return false;
